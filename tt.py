2f7e9c4b775abadb2610f88aafde749c249be3e2
usung <usung418@naver.com>
2024-01-31 22:03:26 +0900

    first

--- tt.py
+++ tt.py
@@ -7,4 +7,10 @@ ssh -i ~/django-portfolio-key.pem ubuntu@3.37.197.249
 pip install 'django<5' 'pillow<10' 'django-admin-thumbnails<0.3'
 ㅋㅋㅋㅋㅋㅋ
 ㅋㅋㅎㅎㅎㄴㅇㅎㄴㅇㅎㄴㅇㅎ
+git init
+git add .
+git commit -m ""
+git remote add origin https://github.com/usung1/Portfolio.git
+git push -u origin main
+
 """
