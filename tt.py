67bb4b5a6f06243de8ce6213246c10f7205db7ff
usung <usung418@naver.com>
2024-01-31 22:15:53 +0900

    rgsr

--- tt.py
+++ tt.py
@@ -13,4 +13,8 @@ git commit -m ""
 git remote add origin https://github.com/usung1/Portfolio.git
 git push -u origin main
 
+git pull origin main --allow-unrelated-histories
+이 명령어는 서로 다른 히스토리를 갖는 브랜치를 병합할 때 사용됩니다. 이를 사용하면 서로 다른 히스토리를 갖는 브랜치를 병합할 수 있습니다.
+
+git clone https://github.com/usung1/Portfolio.git mynewre
 """
