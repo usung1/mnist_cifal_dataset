de83713c74cd16ae4dd394d193edb1e5467601c7
usung <usung418@naver.com>
2024-01-31 21:54:09 +0900

    second

--- tt.py
+++ tt.py
@@ -5,5 +5,6 @@
 scp -i ~/django-portfolio-key.pem -r MyResume ubuntu@3.37.197.249:/home/ubuntu
 ssh -i ~/django-portfolio-key.pem ubuntu@3.37.197.249
 pip install 'django<5' 'pillow<10' 'django-admin-thumbnails<0.3'
-sdfsdfsdfsdfdsfsd
+ㅋㅋㅋㅋㅋㅋ
+ㅋㅋㅎㅎㅎ
 """
